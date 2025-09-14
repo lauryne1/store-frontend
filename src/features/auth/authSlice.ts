00d2ac294026f1239a176ae9612ab5574d5c1f9d
lauryne1 <laurynejosiasdady@gmail.com>
2025-09-14 16:19:24 +0100

    add feactures in app

---
+++ src/features/auth/authSlice.ts
@@ -0,0 +1,28 @@
+import { createSlice} from "@reduxjs/toolkit"
+import { Users } from "lucide-react"
+
+const authSlice = createSlice({
+    name: "auth",
+    initialState: {
+        token:null,
+        user: null,
+    },
+    reducers:{
+        setCredentials:(state, action) =>{
+            const{user,accessToken}=action.payload
+            state.user = user
+            state.token = accessToken
+        },
+        logOut:(state, action)=>{
+            state.user = null
+            state.token = null
+
+        },
+    }
+})
+export const{setCredentials, logOut} = authSlice.actions
+
+export default authSlice.reducer
+
+export const selectCurrentUser = (state:any) => state.auth.user
+export const selectCurrentToken = (state:any) => state.auth.token
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { store } from './app/api/store.js'
+
+import { Provider } from 'react-redux'
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+         <BrowserRouter>
+         <Routes>
+          <Route path="/*" element={<App /> }/>
+        </Routes>
+     </BrowserRouter>
+    </Provider>
   </StrictMode>,
 )
